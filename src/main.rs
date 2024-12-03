d24ba9698f069e53b127f106f2a1741db56cf1a0
Brian Davenport <breander@gmail.com>
2024-12-03 12:57:59 -0600

    Part 2 finished. Cleaned up code.

--- src/main.rs
+++ src/main.rs
@@ -17,28 +17,42 @@ fn main() {
     let lines = buffer.lines();
     let mut part1_safe = 0;
     let mut part2_safe = 0;
-    //let mut line_count = 0;
+    let mut line_count = 0;
 
     // read through the lines 
     for line in lines {
-        let mut parts = line.split(" ").collect::<Vec<&str>>();
+        let parts = line.split(" ").collect::<Vec<&str>>();
         
         if is_line_safe(&parts) {
-            //println!("line {}: is {}", line_count, is_safe);
+            println!("✅ line {}: {}", line_count, line);
             part1_safe += 1;
             part2_safe += 1;
         }
         else {
-            // remove first element to cause line to be unsafe
-            let pos = position_of_first_unsafe_element(&parts);
-            parts.remove(pos);
-        
-            // then recheck if line safe, if now safe then add to part 2 safe
-            if is_line_safe(&parts) {
-                part2_safe += 1;
+            println!("❌ line {}: {}", line_count, line);
+           
+            //try each version of the line with one element removed and see if it allows the line
+            //to pass inspection
+            let length = parts.len();
+            for pos in 0..length {
+                let mut shortned: Vec<&str> = Vec::new();
+                for(i, el) in parts.iter().enumerate() {
+                    if i != pos {
+                        shortned.push(el);
+                    }
+                }
+                let short = shortned.join(" ");
+                if is_line_safe(&shortned) {
+                    part2_safe += 1;
+                    println!("✅ line {}: {}", line_count, short);
+                    break;
+                }
+                else {
+                    println!("❌ line {}: {}", line_count, short);
+                } 
             }
         }
-        //line_count += 1;
+        line_count += 1;
     }
 
     println!("part 1 safe: {}", part1_safe);
@@ -48,19 +62,15 @@ fn main() {
 fn is_line_safe(parts: &Vec<&str>) -> bool {
     let mut count = 0;
     let length = parts.len();
-    //println!("length: {}", length);
     let mut direction = "";
     let mut is_safe = true;
 
     for part in parts {
-       //println!("pos: {} - {}", count, *part); 
        if count + 1 >= length {
-           //println!("End of Line!");
            continue;
        }
        let left = part.parse::<i32>().unwrap();
        let right = parts[count+1].parse::<i32>().unwrap();
-       //println!("left: {} - right: {}", left, right);
        let mut current_direction = "<";
 
        if left > right {
@@ -88,44 +98,3 @@ fn is_line_safe(parts: &Vec<&str>) -> bool {
     return is_safe;
 }
 
-// get the position of the first element that causes line to be unsafe
-fn position_of_first_unsafe_element(parts: &Vec<&str>) -> usize {
-    let mut pos = 0;
-    //let mut pos_to_remove = 0;
-    let length = parts.len();
-    let mut direction = "";
-
-    for part in parts {
-        if pos + 1 >= length {
-            continue;
-        }
-
-       let left = part.parse::<i32>().unwrap();
-       let right = parts[pos+1].parse::<i32>().unwrap();
-        
-       let mut current_direction = "<";
-
-       if left > right {
-          current_direction = ">";
-       }
-
-       if direction == "" {
-           direction = current_direction;
-       }
-
-       if direction != current_direction {
-           //pos_to_remove = pos;
-           break;
-       }
-
-       let distance = (left - right).abs();
-
-       if distance < 1 || distance > 3 {
-           //pos_to_remove = pos;
-           break;
-       }
-       pos += 1;
-    }
-
-    return pos;
-}
